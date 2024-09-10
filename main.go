cb7c0c38057ad1e7aae281ab81fa4af35470cae9
diwakarsinghtest <151877229+diwakarsinghtest@users.noreply.github.com>
2024-09-11 01:23:04 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello world")
 	fmt.Fprintf(w, "Hello, World!")
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	// A very simple health check.
 	w.WriteHeader(http.StatusOK)
+	fmt.Println("heatlh cjecl")
 	w.Header().Set("Content-Type", "application/json")
 	// In the future we could report back on the status of our DB, or our cache
 	// (e.g., Redis) by performing a simple PING, and include them in the response.
@@ -22,5 +24,9 @@ func main() {
 	http.HandleFunc("/home", handlerFunc)
 	http.HandleFunc("/health", healthCheckHandler)
 	fmt.Println("http service running")
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
 }
